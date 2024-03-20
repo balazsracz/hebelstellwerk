088287f9718aa2bdf6d3e34187f6f81f2eea7b65
Balazs Racz <balazs.racz@gmail.com>
2024-03-20 09:00:28 +0100

    Updates file header.

--- src/stw/TurnoutLever.h
+++ src/stw/TurnoutLever.h
@@ -24,12 +24,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * \file Executor.h
+ * \file TurnoutLever.h
  *
- * Class to control execution in the polling loop.
+ * Implements the state machine for a single turnout lever.
  *
  * @author Balazs Racz
- * @date 7 Mar 2024
+ * @date 19 Mar 2024
  */
 
 #ifndef _STW_TURNOUTLEVER_H_
