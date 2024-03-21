57208ebb03416673fa719664c1440bc9f05b8f59
Balazs Racz <balazs.racz@gmail.com>
2024-03-21 21:40:27 +0100

    Adds declarations for the lock table. Adds helper function to find a row.

---
+++ src/stw/LockTable.h
@@ -0,0 +1,142 @@
+/** \copyright
+ * Copyright (c) 2024, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file LockTable.h
+ *
+ * Declarations for representing the lock table (Verschlusstabelle).
+ *
+ * @author Balazs Racz
+ * @date 21 Mar 2024
+ */
+
+#ifndef _STW_LOCKTABLE_H_
+#define _STW_LOCKTABLE_H_
+
+#include <initializer_list>
+#include <cstddef>
+#include <utility>
+
+#include "stw/Types.h"
+#include "utils/Singleton.h"
+
+enum LockTableEntryType : uint8_t {
+  EOF,
+  ROUTE_ROW,
+  TURNOUT_PLUS,
+  TURNOUT_MINUS,
+  SIGNAL_HP1,
+  SIGNAL_F1 = SIGNAL_HP1,
+  SIGNAL_HP2,
+  SIGNAL_F2 = SIGNAL_HP2,
+  AUX_PLUS,
+  BLOCK_OUT,
+  BLOCK_IN,
+};
+
+struct LockTableEntry {
+  constexpr LockTableEntry(LockTableEntryType type, uint8_t arg)
+      : type_(type), arg_(arg) {}
+  LockTableEntryType type_;
+  uint8_t arg_;
+
+  bool operator!=(const LockTableEntry o) const {
+    return type_ != o.type_ || arg_ != o.arg_;
+  }
+};
+
+static constexpr LockTableEntry lock_table_helper(LockTableEntryType type,
+                                                  uint8_t value) {
+  return LockTableEntry(type, value);
+  // (((uint16_t)type) << 8) | value;
+}
+
+static constexpr LockTableEntry Route(RouteId id) {
+  return lock_table_helper(ROUTE_ROW, id);
+}
+
+static constexpr LockTableEntry TurnoutPlus(TurnoutId id) {
+  return lock_table_helper(TURNOUT_PLUS, id);
+}
+
+static constexpr LockTableEntry WeichePlus(TurnoutId id) {
+  return lock_table_helper(TURNOUT_PLUS, id);
+}
+
+static constexpr LockTableEntry TurnoutMinus(TurnoutId id) {
+  return lock_table_helper(TURNOUT_MINUS, id);
+}
+
+static constexpr LockTableEntry WeicheMinus(TurnoutId id) {
+  return lock_table_helper(TURNOUT_MINUS, id);
+}
+
+static constexpr LockTableEntry Hp1(SignalId id) {
+  return lock_table_helper(SIGNAL_HP1, id);
+}
+
+static constexpr LockTableEntry Hp2(SignalId id) {
+  return lock_table_helper(SIGNAL_HP2, id);
+}
+
+/// Instantiate this class to provide the lock table (Verschlusstabelle). The
+/// necessary syntax is this:
+///
+/// LockTable tbl({ //
+///   Route(ROUTE_A1), TurnoutPlus(TURNOUT_1), Hp2(SIGNAL_A), //
+///   Route(ROUTE_B1), TurnoutMinus(TURNOUT_8), Hp1(SIGNAL_C), //
+/// });
+class LockTable : public Singleton<LockTable> {
+ public:
+  LockTable(std::initializer_list<LockTableEntry> entries)
+      : ar_(entries) {}
+
+  const LockTableEntry* begin() {
+    return ar_.begin();
+  }
+
+  const LockTableEntry* end() {
+    return ar_.end();
+  }
+
+  std::pair<const LockTableEntry*, size_t> find_route(RouteId id) {
+    const LockTableEntry* ptr;
+    for (ptr = begin(); ptr != end() && *ptr != Route(id); ++ptr);
+    if (ptr == end()) {
+      return {nullptr, 0};
+    }
+    ++ptr;
+    const LockTableEntry* eptr;
+    for (eptr = ptr; eptr != end() && eptr->type_ != ROUTE_ROW; ++eptr);
+    return {ptr, eptr - ptr};
+  }
+  
+ private:
+  std::initializer_list<LockTableEntry> ar_;
+};
+
+using Verschlusstabelle = LockTable;
+
+#endif  // _STW_LOCKTABLE_H_
