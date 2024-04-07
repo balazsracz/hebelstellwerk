6c1b9673594ce4944c622e2c622a1786eb0670ea
Balazs Racz <balazs.racz@gmail.com>
2024-04-07 17:42:36 +0200

    Felderblock: adds gpio accessors and setup functions. Adds state enum. Writes the output fields based on the state.

--- src/stw/FelderBlock.h
+++ src/stw/FelderBlock.h
@@ -37,6 +37,7 @@
 
 #include "stw/Block.h"
 #include "stw/I2CBlock.h"
+#include "utils/Timer.h"
 
 class FelderBlock : public Block {
  public:
@@ -48,6 +49,13 @@ class FelderBlock : public Block {
               route_lock_button_pin, route_lock_button_inverted,
               route_locked_lamp_pin, route_locked_lamp_inverted) {}
 
+  // Setup functions. Allows defining the GPIO mapping for this given block in
+  // a way that is a bit more readable, than just having a super long list of
+  // numbers in the constructor. The return values should be combined with |,
+  // and at the end the resulting bitmask should equal the constant
+  // EXPECTED_SETUP.
+
+  
   uint16_t set_vorblock_taste(gpio_pin_t pin, bool inverted) {
     vorblock_taste_.setup(pin, inverted, GPIO_INPUT);
     return 1u<<0;
@@ -65,19 +73,129 @@ class FelderBlock : public Block {
     return 1u<<3;
   }
 
+  uint16_t set_anfangsfeld(gpio_pin_t pin, bool inverted) {
+    anfangsfeld_.setup(pin, inverted, GPIO_OUTPUT);
+    return 1u<<4;
+  }
+  uint16_t set_endfeld(gpio_pin_t pin, bool inverted) {
+    endfeld_.setup(pin, inverted, GPIO_OUTPUT);
+    return 1u<<5;
+  }
+  uint16_t set_erlaubnisfeld(gpio_pin_t pin, bool inverted) {
+    erlaubnisfeld_.setup(pin, inverted, GPIO_OUTPUT);
+    return 1u<<6;
+  }
 
+  uint16_t set_signalhaltmelder(gpio_pin_t pin, bool inverted) {
+    signalhaltmelder_.setup(pin, inverted, GPIO_OUTPUT);
+    return 1u<<7;
+  }
+  uint16_t set_storungsmelder(gpio_pin_t pin, bool inverted) {
+    storungsmelder_.setup(pin, inverted, GPIO_OUTPUT);
+    return 1u<<8;
+  }
+  
   /// These bits should be set after all the setup is done.
-  static constexpr uint16_t EXPECTED_SETUP = (1u<<5) - 1;
+  static constexpr uint16_t EXPECTED_SETUP = (1u<<9) - 1;
 
+  enum class State : uint8_t {
+    /// No permission, track is free. 
+    IN_FREE,
+    /// No permission, track is occupied. 
+    IN_OCC,
+    /// Have permission, track is free. 
+    OUT_FREE,
+    /// Have permission, track is occupied. 
+    OUT_OCC
+  };
+  
 private:
+  void begin() override {
+    Block::begin();
+    tm_.start_drifting(13);
+  }
+
+  void loop() override {
+    Block::loop();
+    // Sets the output GPIOs.
+    switch(state_) {
+      case State::IN_FREE: {
+        /// @todo is the Anfangsfeld red when the Erlaubnisfeld is red?
+        anfangsfeld_.write(WHITE);
+        endfeld_.write(WHITE);
+        erlaubnisfeld_.write(RED);
+        break;
+      }
+      case State::IN_OCC: {
+        /// @todo is the Anfangsfeld red when the Erlaubnisfeld is red?
+        anfangsfeld_.write(WHITE);
+        endfeld_.write(RED);
+        erlaubnisfeld_.write(RED);
+        break;
+      }
+      case State::OUT_FREE: {
+        anfangsfeld_.write(WHITE);
+        endfeld_.write(WHITE);
+        erlaubnisfeld_.write(WHITE);
+        break;
+      }
+      case State::OUT_OCC: {
+        anfangsfeld_.write(RED);
+        endfeld_.write(WHITE);
+        erlaubnisfeld_.write(WHITE);
+        break;
+      }
+    }
+    // The rest of the logic we only do occasionally.
+    if (!tm_.check()) {
+      return;
+    }
+    
+  }
+
+  
   /// Hardware connection for the actual block PCB.
   I2CBlockInterface* iface_;
 
+  /// Button to signal an outgoing train to the next station, i.e., set the
+  /// track to busy with an outgoing train.
   DelayedGpioAccessor vorblock_taste_;
+  /// Button to signal that an incoming train from the next station has
+  /// arrived, i.e., to set a busy incoming track to clear.
   DelayedGpioAccessor ruckblock_taste_;
+  /// Button to hand off the permission to the next station, i.e., set an
+  /// outgoing track to an incoming track.
   DelayedGpioAccessor abgabe_taste_;
 
+  /// Input for the crank. For an analog crank, this should be a virtual Gpio
+  /// using the AnalogGpio class.
   DelayedGpioAccessor kurbel_;
+
+  static constexpr bool RED = false;
+  static constexpr bool WHITE = true;
+  
+  /// Output for a red/white field showing that an outgoing track is
+  /// clear. Value is 1 for white, 0 for red.
+  DelayedGpioAccessor anfangsfeld_;
+  /// Output for a red/white field showing that an incoming track is
+  /// clear. Value is 1 for white, 0 for red.
+  DelayedGpioAccessor endfeld_;
+  /// Output for a red/white field showing that we have the permission to send
+  /// trains to the track. Value is 1 for white, 0 for red.
+  DelayedGpioAccessor erlaubnisfeld_;
+
+  /// Output for a red field showing that the incoming signal is showing the
+  /// stop aspect. 1 for red, 0 for dark.
+  DelayedGpioAccessor signalhaltmelder_;
+  /// Output for a field showing that there is an error. Value is 1 for error,
+  /// 0 for no error.
+  DelayedGpioAccessor storungsmelder_;
+
+  Timer tm_;
+  
+  /// Current state of the block.
+  State state_ {State::IN_OCC};
+
   
 };
 
