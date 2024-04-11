fdf9dafb943baa918d13d1b4ce7a59a6d0a6ce42
Balazs Racz <balazs.racz@gmail.com>
2024-04-11 22:28:38 +0200

    Adds dual version of PWMGpio.

--- src/utils/PwmGpio.h
+++ src/utils/PwmGpio.h
@@ -90,4 +90,73 @@ class PwmGpio : public Gpio {
   mutable bool last_state_{false};
 };
 
+/// Instantiate this class when driving a dual-color object with two LEDs
+/// connected to two PWM pins. In the ON state one LED will be driven, in the
+/// OFF state the other. The duty cycle percent will be a parameter for the on
+/// and off outputs.
+class DualPwmGpio : public Gpio {
+ public:
+  /// Constructor.
+  ///
+  /// @param gpio_pin pin number to use for registering this virtual GPIO pin.
+  /// @param pwm_pin_off pin number for the PWM pin to drive when the virtual
+  /// gpio is off (zero).
+  /// @param pwm_pin_on pin number for the PWM pin to drive when the virtual
+  /// gpio is on (one).
+  /// @param pct_off percent (0..100) duty cycle to use when the GPIO is
+  /// commanded to off.
+  /// @param pct_on percent (0..100) duty cycle to use when the GPIO is
+  /// commanded to on.
+  DualPwmGpio(gpio_pin_t gpio_pin, pwm_pin_t pwm_pin_off, pwm_pin_t pwm_pin_on,
+              int8_t pct_off, int8_t pct_on)
+      : pwm_pin_off_(pwm_pin_off),
+        pwm_pin_on_(pwm_pin_on),
+        pct_off_(pct_off),
+        pct_on_(pct_on) {
+    GpioRegistry::instance()->register_obj(this, gpio_pin);
+    pwm_off_ = PwmRegistry::instance()->get(pwm_pin_off_);
+    pwm_on_ = PwmRegistry::instance()->get(pwm_pin_on_);
+  }
+
+  void set_output(gpio_pin_t pin) const override {
+    // Nothing to do, always an output.
+  }
+
+  void write(gpio_pin_t pin, bool value) const override {
+    if (value != last_state_) {
+      last_state_ = value;
+      uint32_t count = pwm_on_->tick_per_period();
+      if (last_state_) {
+        count *= pct_on_;
+        count /= 100;
+        pwm_on_->write(pwm_pin_on_, count);
+        pwm_off_->write(pwm_pin_off_, 0);
+      } else {
+        count *= pct_off_;
+        count /= 100;
+        pwm_off_->write(pwm_pin_off_, count);
+        pwm_on_->write(pwm_pin_on_, 0);
+      }
+    }
+  }
+
+  bool read(gpio_pin_t pin) const override { return last_state_; }
+
+ private:
+  /// PWM pin object from the registry.
+  const Pwm* pwm_off_;
+  /// PWM pin object from the registry.
+  const Pwm* pwm_on_;
+  /// Number of the PWM pin to drive.
+  pwm_pin_t pwm_pin_off_;
+  /// Number of the PWM pin to drive.
+  pwm_pin_t pwm_pin_on_;
+  /// Percent duty cycle to drive the PWM pin when the gpio is commanded off.
+  int8_t pct_off_;
+  /// Percent duty cycle to drive the PWM pin when the gpio is commanded on.
+  int8_t pct_on_;
+  /// Last commanded state of the GPIO pin.
+  mutable bool last_state_{false};
+};
+
 #endif  // _UTILS_PWMGPIO_H_
